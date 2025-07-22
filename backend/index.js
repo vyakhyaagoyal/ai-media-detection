c40316ef7f36882a1105f4cad0fc7296c6874ad4
Vyakhya Goyal <vyakhyatwentytwo@gmail.com>
2025-07-23 02:18:02 +0530

    multer

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,28 @@
 require('dotenv').config();
-const connectToMongo=require('./db');
-const express=require('express');
-const cors=require('cors');
+const connectToMongo = require('./db');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
 
 connectToMongo();
-const app=express();
-const port=5000;
+const app = express();
+const port = 5000;
+const upload = multer({ dest: 'uploads/' }) //upload is a middleware
 app.use(cors());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Hello from server');
 })
 
 app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
-app.listen(port,()=>{
-console.log(`App listening on port ${port}`);
+app.post('/upload', upload.single('profileImage'), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    res.send('Succesfully uploaded');
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
 })
--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 // import './App.css';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  useEffect(() => {
-    axios.get('http://localhost:5000/')
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch(error => {
-        console.error(error);
+  const [file, setFile] = useState(null);
+
+  // useEffect(() => {
+  //   axios.get('http://localhost:5000/')
+  //     .then((res) => {
+  //       console.log(res.data);
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // }, []);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleUpload = async(e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('profileImage', file);
+
+    if (!file) {
+      alert('Please select a file first');
+      return;
+    }
+
+    try {
+      const res = await axios.post('http://localhost:5000/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
       });
-  }, []);
+      console.log(res.data);
+      alert('Upload successful');
+    } catch (err) {
+      console.error(err);
+      alert('Upload failed');
+    }
+  };
 
   return (
     <div>
-      
+      <form onSubmit={handleUpload}>
+        <input type='file' onChange={handleFileChange}/>
+        <button type='submit'>Upload</button>
+      </form>
     </div>
   );
 }
