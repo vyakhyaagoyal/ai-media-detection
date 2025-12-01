139237f41c3c41252a447a3270c59b260fd78fd8
Vyakhya Goyal <vyakhyatwentytwo@gmail.com>
2025-12-02 01:44:58 +0530

    added mongodb atlas'

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,28 @@
-const mongoose = require("mongoose");
-const mongoURI = process.env.MONGO_URI;
+// const mongoose = require("mongoose");
+// const mongoURI = process.env.MONGO_URI;
 
-const connectToMongo = () => {
-    try {
-        mongoose.connect(mongoURI);
-        console.log("connected to mongo");
-    }
-    catch(error){
-        console.error("Error connecting to MongoDB:", error.message);
-    }
-}
+// const connectToMongo = () => {
+//     try {
+//         mongoose.connect(mongoURI);
+//         console.log("connected to mongo");
+//     }
+//     catch(error){
+//         console.error("Error connecting to MongoDB:", error.message);
+//     }
+// }
 
-module.exports = connectToMongo;
+// module.exports = connectToMongo;
+
+import mongoose from "mongoose";
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectToMongo;
